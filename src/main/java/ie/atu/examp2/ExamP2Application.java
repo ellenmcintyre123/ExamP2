0c9123c1b467c3279a166c66e358aec8b28a31ea
ellenmcintyre123 <g00389482@atu.ie>
2023-11-07 16:24:02 +0000

    warehouse part

---
+++ src/main/java/ie/atu/examp2/ExamP2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.examp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamP2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamP2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/examp2/ExamP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.examp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamP2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
